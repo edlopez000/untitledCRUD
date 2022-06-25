32a954070da54a9bee2c531b13729beecc19aedd
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-06-25 03:49:26 -0400

    Forward/Backwards buttons disabled on data edge

--- src/components/PoemActions.tsx
+++ src/components/PoemActions.tsx
@@ -1,20 +1,30 @@
 import { Box, Button, Paper, Stack } from '@mui/material';
+import Poem from '../models/poems';
 import React from 'react';
 
 interface PoemActionsProps {
+  poems: Poem[];
+  poemIndex: number;
   loadNewPoem: () => void;
   nextPoem: () => void;
   prevPoem: () => void;
 }
 
 export default function PoemActions({
+  poems,
+  poemIndex,
   loadNewPoem,
   nextPoem,
   prevPoem,
 }: PoemActionsProps) {
   return (
     <Box display={'flex'} justifyContent={'center'}>
-      <Button variant={'contained'} sx={{ mx: 1 }} onClick={() => prevPoem()}>
+      <Button
+        disabled={poemIndex === 0}
+        variant={'contained'}
+        sx={{ mx: 1 }}
+        onClick={() => prevPoem()}
+      >
         Previous
       </Button>
       <Button
@@ -24,7 +34,12 @@ export default function PoemActions({
       >
         Get New Poem
       </Button>
-      <Button variant={'contained'} sx={{ mx: 1 }} onClick={() => nextPoem()}>
+      <Button
+        disabled={poemIndex === poems.length - 1}
+        variant={'contained'}
+        sx={{ mx: 1 }}
+        onClick={() => nextPoem()}
+      >
         Next
       </Button>
     </Box>
